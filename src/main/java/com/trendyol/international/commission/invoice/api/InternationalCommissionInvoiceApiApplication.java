53eba4007148db4f32fe7582171eaf3e755e5cc9
Fatih Kaan Tuncer <kaan.tuncer@trendyol.com>
2022-02-11 12:53:49 +0300

    SF-1274 itext html2pdf poc initial tests removed

--- src/main/java/com/trendyol/international/commission/invoice/api/InternationalCommissionInvoiceApiApplication.java
+++ src/main/java/com/trendyol/international/commission/invoice/api/InternationalCommissionInvoiceApiApplication.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
 @RestController
 public class InternationalCommissionInvoiceApiApplication {
-
-	@GetMapping("/")
-	String home() {
-		return "Spring is here!";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(InternationalCommissionInvoiceApiApplication.class, args);
 	}
--- src/test/java/com/trendyol/international/commission/invoice/api/InternationalCommissionInvoiceApiApplicationTests.java
+++ src/test/java/com/trendyol/international/commission/invoice/api/InternationalCommissionInvoiceApiApplicationTests.java
@@ -3,9 +3,6 @@ package com.trendyol.international.commission.invoice.api;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class InternationalCommissionInvoiceApiApplicationTests {
@@ -14,12 +11,4 @@ public class InternationalCommissionInvoiceApiApplicationTests {
 	public void contextLoads() {
 	}
 
-	@Autowired
-	private TestRestTemplate restTemplate;
-
-	@Test
-	public void homeResponse() {
-		String body = this.restTemplate.getForObject("/", String.class);
-		assertThat(body).isEqualTo("Spring is here!");
-	}
 }
