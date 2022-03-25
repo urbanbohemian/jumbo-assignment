8033a486372f8aab8f307aa0d9cf2d26ec8926c8
Okan Uslu <okan.uslu@trendyol.com>
2022-03-25 16:12:25 +0300

    SF-1342 swagger config format

---
+++ src/main/java/com/trendyol/international/commission/invoice/api/config/kafka/swagger/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.trendyol.international.commission.invoice.api.config.kafka.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(info = @Info(
+        title = "${spring.application.name}",
+        description = "${spring.application.description}",
+        termsOfService = "${spring.application.termsOfService}"
+))
+@Configuration
+public class SwaggerConfig {
+}
