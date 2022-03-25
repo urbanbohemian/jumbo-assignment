5c12712bb09e4f35e1081b1fee8a70245213933d
Okan Uslu <okan.uslu@trendyol.com>
2022-03-25 14:22:25 +0300

    SF-1342 swagger config change from springfox to springdoc

--- src/main/java/com/trendyol/international/commission/invoice/api/InternationalCommissionInvoiceApiApplication.java
+++ src/main/java/com/trendyol/international/commission/invoice/api/InternationalCommissionInvoiceApiApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableConfigurationProperties
 @EnableJpaAuditing
 public class InternationalCommissionInvoiceApiApplication {
--- src/main/java/com/trendyol/international/commission/invoice/api/controller/monitoring/IndexController.java
+++ src/main/java/com/trendyol/international/commission/invoice/api/controller/monitoring/IndexController.java
@@ -4,9 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
-import springfox.documentation.annotations.ApiIgnore;
 
-@ApiIgnore
 @RestController
 @RequestMapping("/")
 public class IndexController {
