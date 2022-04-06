f1528b5820e322541724f32dc85d79f87d7f6112
Okan Uslu <okan.uslu@trendyol.com>
2022-04-06 16:33:00 +0300

    SF-1343 code refactoring

--- src/main/java/com/trendyol/international/commission/invoice/api/InternationalCommissionInvoiceApiApplication.java
+++ src/main/java/com/trendyol/international/commission/invoice/api/InternationalCommissionInvoiceApiApplication.java
@@ -17,5 +17,4 @@ public class InternationalCommissionInvoiceApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InternationalCommissionInvoiceApiApplication.class, args);
 	}
-
 }
