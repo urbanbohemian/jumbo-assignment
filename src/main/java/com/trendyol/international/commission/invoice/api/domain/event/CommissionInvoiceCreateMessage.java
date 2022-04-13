aa96f845a283b74f5e769defea64dce9adfd1709
Okan Uslu <okan.uslu@trendyol.com>
2022-04-13 10:24:40 +0300

    1376 code fix

--- src/main/java/com/trendyol/international/commission/invoice/api/domain/event/CommissionInvoiceCreateMessage.java
+++ src/main/java/com/trendyol/international/commission/invoice/api/domain/event/CommissionInvoiceCreateMessage.java
@@ -1,5 +1,6 @@
 package com.trendyol.international.commission.invoice.api.domain.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import java.util.Date;
 @Builder
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class CommissionInvoiceCreateMessage {
     private Long sellerId;
     private String country;
