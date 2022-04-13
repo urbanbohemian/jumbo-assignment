489cf405f3f29c9306cf78878a7247e189b216fe
Okan Uslu <okan.uslu@trendyol.com>
2022-04-13 10:22:04 +0300

    SF-1376 adds default constructor to commission invoice create message

--- src/main/java/com/trendyol/international/commission/invoice/api/domain/event/CommissionInvoiceCreateMessage.java
+++ src/main/java/com/trendyol/international/commission/invoice/api/domain/event/CommissionInvoiceCreateMessage.java
@@ -2,11 +2,13 @@ package com.trendyol.international.commission.invoice.api.domain.event;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Builder
 @Data
+@NoArgsConstructor
 public class CommissionInvoiceCreateMessage {
     private Long sellerId;
     private String country;
