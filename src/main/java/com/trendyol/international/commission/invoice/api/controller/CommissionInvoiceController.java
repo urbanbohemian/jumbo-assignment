803344e4ac48245bf126dbbfa9b517414ca2f163
mert.unsal <mert.unsal@trendyol.com>
2022-04-13 10:28:19 +0300

    SF-1355 adds log to each endpoint

--- src/main/java/com/trendyol/international/commission/invoice/api/controller/CommissionInvoiceController.java
+++ src/main/java/com/trendyol/international/commission/invoice/api/controller/CommissionInvoiceController.java
@@ -4,11 +4,13 @@ import com.trendyol.international.commission.invoice.api.mapper.CommissionInvoic
 import com.trendyol.international.commission.invoice.api.model.request.CommissionInvoiceCreateRequest;
 import com.trendyol.international.commission.invoice.api.service.CommissionInvoiceService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/commission-invoice")
@@ -17,16 +19,22 @@ public class CommissionInvoiceController {
 
     @PostMapping
     public void create(@RequestBody CommissionInvoiceCreateRequest commissionInvoiceCreateRequest) {
+        log.info("Commission Invoice Create Job Execution is started.");
         commissionInvoiceService.create(CommissionInvoiceCreateMapper.INSTANCE.commissionInvoiceCreateDto(commissionInvoiceCreateRequest));
+        log.info("Commission Invoice Create Job Execution is started.");
     }
 
     @PostMapping("/generate-serial-number")
     public void generateSerialNumber() {
+        log.info("Commission Invoice Generate Serial Number Job Execution is started.");
         commissionInvoiceService.generateSerialNumber();
+        log.info("Commission Invoice Generate Serial Number Job Execution is ended successfully.");
     }
 
     @PostMapping("/generate-pdf")
     public void generatePdf() {
+        log.info("Commission Invoice Generate PDF Job Execution is started.");
         commissionInvoiceService.generatePdf();
+        log.info("Commission Invoice Generate PDF Job Execution is ended successfully.");
     }
 }
