a41f453529788ca99c69bb58bfa8cee8b9cc14c0
Okan Uslu <okan.uslu@trendyol.com>
2022-05-05 14:35:54 +0300

    SF-1357 settlement item temp retry implementation, skip-ci

--- src/main/java/com/trendyol/international/commission/invoice/api/domain/event/SettlementItemDebeziumEvent.java
+++ src/main/java/com/trendyol/international/commission/invoice/api/domain/event/SettlementItemDebeziumEvent.java
@@ -1,8 +1,13 @@
 package com.trendyol.international.commission.invoice.api.domain.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.trendyol.international.commission.invoice.api.util.Hashing;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.Optional;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,4 +16,9 @@ public class SettlementItemDebeziumEvent {
     private SettlementItemEvent before;
     private SettlementItemEvent after;
     private String op;
+
+    @JsonIgnore
+    public String getHashId() {
+        return Hashing.md5("TODO!");
+    }
 }
--- src/main/java/com/trendyol/international/commission/invoice/api/kafka/consumer/SettlementItemDebeziumConsumer.java
+++ src/main/java/com/trendyol/international/commission/invoice/api/kafka/consumer/SettlementItemDebeziumConsumer.java
@@ -2,8 +2,9 @@ package com.trendyol.international.commission.invoice.api.kafka.consumer;
 
 import com.newrelic.api.agent.Trace;
 import com.trendyol.international.commission.invoice.api.domain.event.SettlementItemDebeziumEvent;
-import com.trendyol.international.commission.invoice.api.util.mapper.SettlementItemMapper;
+import com.trendyol.international.commission.invoice.api.kafka.failover.KafkaConsumerExceptionService;
 import com.trendyol.international.commission.invoice.api.service.SettlementItemService;
+import com.trendyol.international.commission.invoice.api.util.mapper.SettlementItemMapper;
 import com.trendyol.kafkaconfig.annotation.DependsOnKafkaFactories;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -19,6 +21,7 @@ import org.springframework.stereotype.Component;
 @DependsOnKafkaFactories
 public class SettlementItemDebeziumConsumer {
     private final SettlementItemService settlementItemService;
+    private final KafkaConsumerExceptionService kafkaConsumerExceptionService;
 
     @Trace(dispatcher = true)
     @KafkaListener(
@@ -35,6 +38,27 @@ public class SettlementItemDebeziumConsumer {
             settlementItemService.process(SettlementItemMapper.INSTANCE.settlementItemDto(message.getAfter()));
         } catch (Exception exception) {
             log.error("SettlementItemDebeziumConsumer error occurred: ", exception);
+            throw exception;
+        }
+    }
+
+    @Transactional
+    @Trace(dispatcher = true)
+    @KafkaListener(
+            topics = "${kafka-config.consumers[settlement-item-debezium-consumer].retry-topic}",
+            groupId = "${kafka-config.consumers[settlement-item-debezium-consumer].props[retry-group.id]}",
+            containerFactory = "${kafka-config.consumers[settlement-item-debezium-consumer].factory-bean-name}"
+    )
+    public void consumeRetry(@Payload SettlementItemDebeziumEvent message,
+                             @Header(KafkaHeaders.RECEIVED_PARTITION_ID) Integer partition,
+                             @Header(KafkaHeaders.OFFSET) Long offset,
+                             @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        log.info("SettlementItemDebeziumRetryConsumer consumed with topic: {}, and partition: {}, and offset: {}, and message: {}", topic, partition, offset, message);
+        try {
+            settlementItemService.process(SettlementItemMapper.INSTANCE.settlementItemDto(message.getAfter()));
+            kafkaConsumerExceptionService.deleteException(message.getHashId());
+        } catch (Exception exception) {
+            log.error("SettlementItemDebeziumRetryConsumer error occurred: ", exception);
             throw exception;
         }
     }
