70ee65eab13be04d507baf4ae3e403358dfe6ed4
mert.unsal <mert.unsal@trendyol.com>
2022-05-09 16:57:24 +0300

    SF-1 adds log for testing

--- src/main/java/com/trendyol/international/commission/invoice/api/domain/event/CommissionInvoiceCreateEvent.java
+++ src/main/java/com/trendyol/international/commission/invoice/api/domain/event/CommissionInvoiceCreateEvent.java
@@ -6,10 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Date;
 import java.util.Optional;
 
+@Slf4j
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -23,6 +25,11 @@ public class CommissionInvoiceCreateEvent {
 
     @JsonIgnore
     public String getHashId() {
+        log.info(Optional.ofNullable(sellerId).orElse(0L).toString()
+                .concat(Optional.ofNullable(country).orElse(""))
+                .concat(Optional.ofNullable(currency).orElse(""))
+                .concat(Optional.ofNullable(automaticInvoiceStartDate).orElse(new Date(1L)).toString())
+                .concat(Optional.ofNullable(endDate).orElse(new Date(1L)).toString()));
         return Hashing.md5(Optional.ofNullable(sellerId).orElse(0L).toString()
                 .concat(Optional.ofNullable(country).orElse(""))
                 .concat(Optional.ofNullable(currency).orElse(""))
